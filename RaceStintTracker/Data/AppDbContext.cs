c1e6b4506757ead8d834b952f6b932a5e0fb6d88
Noisy_Bomb <vladdimirylanov98@gmail.com>
2026-06-05 22:01:23 +0300

    Remove appsettings.Development.json from tracking

---
+++ RaceStintTracker/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore; //библиотека для бд
+using RaceStintTracker.Models; //библиотека с моделями из models
+
+namespace RaceStintTracker.Data;
+
+public class AppDbContext : DbContext //DbContext - класс из фрейма для общения с бд
+{
+    public AppDbContext (DbContextOptions<AppDbContext> options) : base(options){ } //конструктор, настройки будут приходить из ependency injection
+    public DbSet<Race> Races { get; set; } //представления таблицы в бд, передали Driver и будет таблица с колонками Id, DriverName
+    public DbSet<Driver> Drivers { get; set; }
+    public DbSet<Stint> Stints { get; set; }
+}
