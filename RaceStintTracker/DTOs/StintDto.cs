10791fb7c8aa0a3b62313ce48cccd2e254b1be19
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-06-09 02:49:43 +0300

    StintDto.cs created to make logs clearer

---
+++ RaceStintTracker/DTOs/StintDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RaceStintTracker.DTOs;
+
+public class StintDto
+{
+    public int Id { get; set; }
+    public int StintNumber { get; set; }
+    public string DriverName { get; set; } = string.Empty;
+    public int Laps { get; set; }
+    public TimeSpan StintStartTime { get; set; }
+    public TimeSpan StintEndTime { get; set; }
+}
